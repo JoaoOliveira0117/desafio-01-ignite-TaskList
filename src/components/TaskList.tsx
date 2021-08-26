9c05138dd13d079a0906c30c46667437a943dd9c
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2021-08-25 22:24:09 -0300

    adicionar funcoes de add, remove e complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    let task: Task = {
+      id: Math.floor(100000 + Math.random() * 900000),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    newTaskTitle.replace(/\s+/g, '') != '' ? setTasks([...tasks,task]) : '';
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let taskFilter = [...tasks];
+    taskFilter.find(task => {
+      task.id == id ? task.isComplete = !task.isComplete : ''
+    })
+
+    setTasks(taskFilter);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    let task = [...tasks];
+    let taskFilter = task.filter((task)=>task.id !== id)
+
+    setTasks(taskFilter);
   }
 
   return (
